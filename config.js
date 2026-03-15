726c71474676afc56365e196c0946173efcdea9b
weijinpro <github@weijin.pro>
2026-03-15 20:31:53 +0800

    Update and rename config.js.demo to config.js

---
+++ config.js
@@ -0,0 +1,79 @@
+// --- 本地音乐（用） ---
+var localMusic = [
+  {
+    name: '换到千般恨',
+    artist: '韦伦',
+    url: './music/huandaoqianbanhen.mp3',
+    cover: './music/weilun.png',
+    lrc: './music/huandaoqianbanhen.lrc'
+  },
+  {
+    name: '旧梦不须记',
+    artist: '雷安娜',
+    url: './music/jiumengbuxuji.mp3',
+    cover: './music/jiumengbuxuji.png',
+    lrc: './music/jiumengbuxuji.lrc'
+  },
+
+  // 你可以从这里开始继续添加 ↓↓↓
+
+  {
+    name: '对你说',
+    artist: '王筝',
+    url: './music/duinishuo.mp3',
+    cover: './music/duinishuo.png',
+    lrc: './music/duinishuo.lrc'
+  },
+  {
+    name: '我们都是好孩子,
+    artist: '王筝',
+    url: './music/womendoushihaohaizi.mp3',
+    cover: './music/womendoushihaohazi.png',
+    lrc: './music/womendoushihaohaizi.lrc'
+  },
+  {
+    name: '越单纯越幸福',
+    artist: '王筝',
+    url: './music/yuedanchunyuexingfu.mp3',
+    cover: './music/yuedanchunyuexingfu.png',
+    lrc: './music/yuedanchunyuexingfu.lrc'
+  },
+  {
+    name: '漫步人生路',
+    artist: '邓丽君',
+    url: './music/manburenshenglu.mp3',
+    cover: './music/manburenshenglu.png',
+    lrc: './music/manburenshenglu.lrc'
+  },
+  {
+    name: '恨在今天再相遇',
+    artist: '方季惟',
+    url: './music/henzaijintianzaixiangyu.mp3',
+    cover: './music/henzaijintianzaixiangyu.png',
+    lrc: './music/henzaijintianzaixiangyu.lrc'
+  },
+  {
+    name: '潇洒走一回',
+    artist: '徐佳莹',
+    url: './music/xiaosazouyihui.mp3',
+    cover: './music/xiaosazouyihui.png',
+    lrc: './music/xiaosazouyihui.lrc'
+  },
+  {
+    name: '潇洒走一回0',
+    artist: '叶倩文',
+    url: './music/xiaosazouyihui0.mp3',
+    cover: './music/xiaosazouyihui0.png',
+    lrc: './music/xiaosazouyihui0.lrc'
+  },
+  {
+    name: '怨苍天变了心',
+    artist: '方季惟',
+    url: './music/yuancangtianbianlexin.mp3',
+    cover: './music/yuancangtianbianlexin.png',
+    lrc: './music/yuancangtianbianlexin.lrc'
+  }
+];
+
+// --- 禁用远程 JSON ---
+var remoteMusic = "";
